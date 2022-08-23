b8f34ccff6d5251ee8b81759aad3eeedd5d3a5d8
mazen kouki <mazenlouki@gmail.com>
2022-08-23 16:58:57 +0100

    bill

--- mazen.js
+++ mazen.js
@@ -2,73 +2,73 @@ var food = [
 
 {
     category:"hamburger",
-    imgSrc:"./food/Cheeseburger.jpg"
-    price : "9 dt"
+    imgSrc:"./food/Cheeseburger.jpg",
+    price : 9 
 },{
     category: "hamburger",
-    imgSrc:"./food/Chicken-Burger.png"
-    price : "9.5 dt"
+    imgSrc:"./food/Chicken-Burger.png",
+    price : 9.5 
 },{
     category:"hamburger",
-    imgSrc:"./food/doublecheese.jpg"
-    price : "11 dt"
+    imgSrc:"./food/doublecheese.jpg",
+    price : 11 
 },{
     category:"pizza",
-    imgSrc:"./food/margharitta.jpg"
-    price : "9 dt"
+    imgSrc:"./food/margharitta.jpg",
+    price : 9 
 },{
     category:"pizza",
-    imgSrc:"./food/mexican.jpg"
-    price : "14 dt"
+    imgSrc:"./food/mexican.jpg",
+    price : 14 
 },{
     category:"pizza",
-    imgSrc:"./food/pizza.jpg"
-    price : "10 dt"
+    imgSrc:"./food/pizza.jpg",
+    price : 10 
 },{
     category:"pizza",
-    imgSrc:"./food/fruit de mer.jpg"
-    price : "17 dt"
+    imgSrc:"./food/fruit de mer.jpg",
+    price : 17 
 },{
     category:"pasta",
-    imgSrc:"./food/pasta carbonara.jpg"
-    price : "16 dt"
+    imgSrc:"./food/pasta carbonara.jpg",
+    price : 16 
 },{
     category:"pasta",
-    imgSrc:"./food/pasta fruit de mer.jpg"
-    price : "23 dt"
+    imgSrc:"./food/pasta fruit de mer.jpg",
+    price : 23 
 },{
     category:"pasta",
-    imgSrc:"./food/sauce blanche.jpg"
-    price : "20 dt"
+    imgSrc:"./food/sauce blanche.jpg",
+    price : 20 
 },{
     category:"sandwiches",
-    imgSrc:"./food/chickensandwich.jpg"
-    price : "10 dt"
+    imgSrc:"./food/chickensandwich.jpg",
+    price : 10 
 },{
     category:"sandwiches",
-    imgSrc:"./food/jambon.jpg"
-    price : "8.5 dt"
+    imgSrc:"./food/jambon.jpg",
+    price : 8.5 
 },
 {
     category:"sandwiches",
-    imgSrc:"./food/tuna.jpg"
-    price : "8 dt"
+    imgSrc:"./food/tuna.jpg",
+    price : 8 
 }, {
     category:"supplements",
-    imgSrc:"./food/cheese.jpg"
-    price : "3 dt"
+    imgSrc:"./food/cheese.jpg",
+    price : 3 
 }, {
     category:"supplements",
-    imgSrc:"./food/french.jpg"
-    price : "4 dt"
+    imgSrc:"./food/french.jpg",
+    price : 4 
 },{
   category:"supplements",
-    imgSrc:"./food/supjambon.jpg" 
-    price : "3 dt"
+    imgSrc:"./food/supjambon.jpg", 
+    price : 3 
 
 }, {category:"supplements",
-    imgSrc:"./food/mushrooms.jpg"
-    price : "2.5 dt"
+    imgSrc:"./food/mushrooms.jpg",
+    price : 2.5 
 }
 
 
@@ -95,10 +95,18 @@ each(array,function(e,i){
 })
 return acc;
 }
+
+
  function renderImages(array){
     each(array, function(e,i){
-        var div=$(`<div class='item ${e.category}'><img src="${e.imgSrc}"><button> "click me" <button></div>`)
-
+        var btn = $(`<button class = "price" > ${e.price} dt </button>`)
+        btn.click(function(l){
+            var p = $(`<p>${e.price}</p>`)
+           $("#secondSpan").append(p)
+        })
+        var div=$(`<div  class='item ${e.category}'><img src="${e.imgSrc}"> </div>`)
+div.append(btn)
+   
           div.appendTo($('#littleSpan') )
           })
  } 
@@ -154,11 +162,27 @@ $("#supplements").on("click", function(){
     
     })  
 
-
-
- 
-
+$("#secondSpan").append($("<h1 id = 'bill'> Bill </h1>"))
 
 
 
+// $(".price", this).click(function(){
+//   var P =  $("<p></p> ")
+  
+//   P.text(this)
+  
+//   P.appendTo($("#secondSpan"))  
 
+// })
+
+// function add (){
+//     var item = $("")
+// }
+
+// $('.price').click(function(){
+//     for(var i =0;i<$('.price').length;i++){
+//         if($(".price")[i].data("clicked",true)){
+
+//         }
+//     }
+// })
