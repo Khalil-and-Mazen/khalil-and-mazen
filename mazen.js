2c16f713749be916ef611cf07d86728c83b232ba
mazen kouki <mazenlouki@gmail.com>
2022-08-24 09:32:11 +0100

    final

--- mazen.js
+++ mazen.js
@@ -138,7 +138,7 @@ return acc;
     each(array, function(e,i){
         var btn = $(`<button class = "price" > ${e.price} dt </button>`)
         btn.click(function(l){
-            var p = $(`<p>${e.id} <span class = "prc">${e.price}</span> dt</p>`)
+            var p = $(`<p class="p">${e.id} <span class = "prc">${e.price}</span> dt</p>`)
            $("#secondSpan").append(p)
         })
         var div=$(`<div  class='item ${e.category}'><img src="${e.imgSrc}"> </div>`)
@@ -202,13 +202,10 @@ $("#supplements").on("click", function(){
 $("#secondSpan").append($("<h1 id = 'bill'> Bill </h1>"))
 
 /*function sum(array){
-
     return reduce(array,function(result,e,i){
   return  result + e
     },0)
-
     }
-
     var result = sum($(".prc"))
     $("#finished").click(function(){
         $("#secondSpan").append(`<p>${result}</p>`)
@@ -226,7 +223,79 @@ function sum2(){
 $("#finished").click(function(){
     $("#secondSpan").append(`<p id = "cost"> your Order cost : ${sum2()} dt </p>`)
 })
+$("#finished").css("float","right")
+$("#finished").css("margin-right","30px")
+$("#finished").css("background-color","black")
+$("#finished").css("color","white")
+$("#finished").css("height","40px")
+$("#finished").css("width","100px")
+$("#pizza").css("display","block")
+$("#pizza").css("height","40px")
+$("#pizza").css("width","150px")
+$("#pizza").css("border","2px solid #BFC0C0;")
+$("#pizza").css("margin","10px 20px 20px 20px")
+$("#pizza").css("color","black")
+$("#pizza").css("test-transform","upperCase")
+$("#pizza").css("text-decoration","none")
+$("#pizza").css("font-size",".8em")
+$("#pizza").css("letter-Spacing","1.5px")
+$("#pizza").css("align-items","center")
+$("#pizza").css("justify-content","center")
+$("#pizza").css("overflow","hidden")
 
+$("#hamburger").css("display","block")
+$("#hamburger").css("height","40px")
+$("#hamburger").css("width","150px")
+$("#hamburger").css("border","2px solid #BFC0C0;")
+$("#hamburger").css("margin","10px 20px 20px 20px")
+$("#hamburger").css("color","black")
+$("#hamburger").css("test-transform","upperCase")
+$("#hamburger").css("text-decoration","none")
+$("#hamburger").css("font-size",".8em")
+$("#hamburger").css("letter-Spacing","1.5px")
+$("#hamburger").css("align-items","center")
+$("#hamburger").css("justify-content","center")
+$("#hamburger").css("overflow","hidden")
 
+$("#sandwiches").css("display","block")
+$("#sandwiches").css("height","40px")
+$("#sandwiches").css("width","150px")
+$("#sandwiches").css("border","2px solid #BFC0C0;")
+$("#sandwiches").css("margin","10px 20px 20px 20px")
+$("#sandwiches").css("color","black")
+$("#sandwiches").css("test-transform","upperCase")
+$("#sandwiches").css("text-decoration","none")
+$("#sandwiches").css("font-size",".8em")
+$("#sandwiches").css("letter-Spacing","1.5px")
+$("#sandwiches").css("align-items","center")
+$("#sandwiches").css("justify-content","center")
+$("#sandwiches").css("overflow","hidden")
 
+$("#pasta").css("display","block")
+$("#pasta").css("height","40px")
+$("#pasta").css("width","150px")
+$("#pasta").css("border","2px solid #BFC0C0;")
+$("#pasta").css("margin","10px 20px 20px 20px")
+$("#pasta").css("color","black")
+$("#pasta").css("test-transform","upperCase")
+$("#pasta").css("text-decoration","none")
+$("#pasta").css("font-size",".8em")
+$("#pasta").css("letter-Spacing","1.5px")
+$("#pasta").css("align-items","center")
+$("#pasta").css("justify-content","center")
+$("#pasta").css("overflow","hidden")
+
+$("#supplements").css("display","block")
+$("#supplements").css("height","40px")
+$("#supplements").css("width","150px")
+$("#supplements").css("border","2px solid #BFC0C0;")
+$("#supplements").css("margin","10px 20px 20px 20px")
+$("#supplements").css("color","black")
+$("#supplements").css("test-transform","upperCase")
+$("#supplements").css("text-decoration","none")
+$("#supplements").css("font-size",".8em")
+$("#supplements").css("letter-Spacing","1.5px")
+$("#supplements").css("align-items","center")
+$("#supplements").css("justify-content","center")
+$("#supplements").css("overflow","hidden")
 
